74c9c7cd7435ea8c6ccf1cd1c2793566fc88e6ac
Peter S. Jaworski <psj529@protonmail.com>
2025-09-03 17:37:22 -0500

    Add files via upload

---
+++ permutit.h
@@ -0,0 +1,16 @@
+#include <ctype.h>
+#include <errno.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#if defined(MSDOS) || defined(_MSDOS) || defined(__DOS__) || defined(__MSDOS__)
+#define MAXN ((size_t) 8)
+#else
+#define MAXN ((size_t) 12)
+#endif
+
+#define FAIL EXIT_FAILURE
+
+#define BUFLEN 82
+
